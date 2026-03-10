761cfe7b9560c44f587a01553ac2c4c9ea545048
Tylerk2565 <176984154+Tylerk2565@users.noreply.github.com>
2026-03-09 22:40:30 -0400

    Fixes to controllers

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,44 +1,65 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     private enum EnemyState
     {
-        isAttacking,
-        isFollowing
+        isChasing,
+        isAttacking
+        
     }
 
     private EnemyState _currentState;
+    private NavMeshAgent _agent;
+    private Transform _player;
+
+    [SerializeField] private float _attackRange = 1.5f;
+    private float _attackDamage;
+    private float _health;
 
     private void Start()
     {
-
+        _currentState = EnemyState.isChasing;
+        _agent = GetComponent<NavMeshAgent>();
+        _player = GameObject.Find("PlayerObject").transform;
     }
 
     private void Update()
     {
         switch (_currentState)
         {
-            case EnemyState.isFollowing:
-                IsFollowing();
+            case EnemyState.isChasing:
+                IsChasing();
                 break;
             case EnemyState.isAttacking:
-                isAttacking();
+                IsAttacking();
                 break;
         }
     }
 
-    public void IsFollowing()
+    private void IsChasing()
     {
-
-
+        if (Vector3.Distance(_agent.transform.position, _player.position) >= _attackRange)
+        {
+            _agent.destination = _player.position;
+        }
+        else
+        {
+            _currentState = EnemyState.isAttacking;
+        }
     }
 
-    public void isAttacking()
+    private void IsAttacking()
     {
-
-
+        if (Vector3.Distance(_agent.transform.position, _player.position) <= _attackRange)
+        {
+            _agent.destination = _player.position;
+        } 
+        else
+        {
+            _currentState = EnemyState.isChasing;
+        }
     }
-
 }
 
